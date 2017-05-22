69b5974781ad26d3a3f15985748b8746380f4c30
IMS-Global <brennick.sci@gmail.com>
2017-05-22 11:08:07 -0600

    heads tails file name changed

--- wallet.rb
+++ wallet.rb
@@ -2,8 +2,10 @@ class Wallet
     attr_accessor :amount
 
     def initialize(amount)
-        @amount = amount
+        @amount = amount.to_f
+    end
 
-        
+    def in_debt?
+      @amount > 0
     end
 end
