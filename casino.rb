0cfb5eec65a1bab1c725b502467ec8b31702d752
Natasha Natarajan <tashanoronha@gmail.com>
2017-05-22 13:31:14 -0600

    Added Casino Menu, error checking and action for menu

--- casino.rb
+++ casino.rb
@@ -16,13 +16,54 @@ class Casino
     casino_menu
   end
 
-  def casino_menu
-  	puts "This is the menu."
-  	# puts "1) Game Selection"
-  	# puts "2) Open your wallet"
-  	# puts "3) Go to the bar"
-  	HeadsTails.new(player)
+def casino_menu
+    @valid_selection = ["1", "2", "3", "4"]
+    puts "*****************************************"
+    puts "*                                       *"
+    puts "*   WELCOME TO THE MYSTIC LAKE CASINO!  *"
+    puts "*                                       *"
+    puts "*   PLEASE MAKE A SELECTION TODAY...    *"   
+    puts "*                                       *"     
+    puts "*   1) Play Heads Tails                 *"
+    puts "*   2) Play Dice                        *"
+    puts "*   3) Check your wallet for money      *"      
+    puts "*   4) Exit                             *"
+    puts "*                                       *"
+    puts "*****************************************"
+    puts
+    @menu_selection = gets.chomp
+    # Check if selection is valid
+    check_valid_menu_choice
+end
+
+def check_valid_menu_choice
+    # Go forward with choice
+    if @valid_selection.include? @menu_selection
+      validate_input
+    else 
+      puts "Invalid selection. Please select something from the menu "
+      casino_menu
+    end
+end
+
+# Case statement for Operator Methods
+def validate_input
+  case @menu_selection
+    when "1"
+    # Call Head Tails Game
+    HeadsTails.new(player)
+    when "2"
+    # Call Dice Game
+    when "3"
+    # Call Check Your Wallet Function
+    when "4"
+      exit
   end
 end
 
+
+
+
+
 Casino.new
+
