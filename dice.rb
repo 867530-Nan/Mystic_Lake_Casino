bb9f857d266f655dfe7abe325c109b4ae17cc1c1
IMS-Global <brennick.sci@gmail.com>
2017-05-23 13:15:57 -0600

    Functions for shared earnings or winnings from each player

--- dice.rb
+++ dice.rb
@@ -1,9 +1,12 @@
 # @author Brennick Langston
 # @version 1.0.0
+# @todo More than one winner detected, split earnings
 
-# require 'Colorize'
-#
-# require_relative 'Wallet'
+require 'pry'
+require 'Colorize'
+require 'tty'
+
+require_relative 'Wallet'
 
 # Rolling Dice Street Game
 class Dice
@@ -11,13 +14,14 @@ class Dice
   attr_accessor :ante, :bet, :players
 
   # Initializer for class Dice
-  # @param players [Array] array of players
+  # @param players [Array] array of player objects
   # @param ante [Float] the ante required to be in the game
   # @return nil
   def initialize(players = [], ante = 20.00)
     @ante = ante
     @players = {}
     players.is_a?(Array) ? join_game(players) : join_game([players])
+    @prompt = TTY::Prompt.new
     show_menu
   end
 
@@ -45,41 +49,51 @@ class Dice
     puts "\n"
     puts format '%3s%s', ' ', 'Hit the [Enter Key] to try your luck!'
     gets
+    # puts "\e[2J"
+    rounds = @prompt.ask("   Enter the number rounds to be played:\s".red, convert: :int)
+    play_round(rounds)
+  end
+
+  # Helper Method for clearing the terminal sceen
+  # @return nil
+  def clear_screen
     puts "\e[2J"
-    puts "   Enter the number rounds to be played:\s".red
-    play_round(gets.strip.to_i)
+  end
+
+  def spacer(new_lines = 1)
+    puts "\n" * new_lines.to_i
   end
 
   # Displays the players amounts that are left in their wallets (aka. cash flow)
   # @return nil
   def show_wallets
-    puts "\e[2J"
-    puts "++++++++++++++++++ Players Wallets ++++++++++++++++++"
+    clear_screen
+    @prompt.say("++++++++++++++++++ Players Wallets ++++++++++++++++++")
     @players.each do |player, data|
       points = @players[player][:points]
       wallet = player.wallet.amount
-      puts "#{player.name}\shas\s$#{wallet} and #{points} points"
+      @prompt.say("#{player.name}\shas\s$#{wallet} and #{points} points")
     end
-    puts "\n"
+    spacer(3)
   end
 
   # Obtains the bet that the player is going to place on the next round of dice
   # @param player [Player] a single player object of the game
   # @return [Integer]
   def place_bet(player)
-    print "What is #{player.name}\'s bet? " +
-      "[ Enter 1 for pass, 2 for don\'t pass, 3 for points]\s"
-    ans = gets.strip.to_i
-    ans
+    clear_screen
+    question = "What is #{player.name}\'s bet? " +
+      "[ Enter: 1 for pass, 2 for don\'t pass, 3 for points]\s"
+    bet = @prompt.ask(question, convert: :int)
+    bet
   end
 
   # Obtains the ante for the next set of rounds from each player
-  # @param [Player] a single player object placing the ante
+  # @param player [Player] a single player object placing the ante
   # @return [Integer]
   def place_ante(player)
-    show_wallets
-    print "What is #{player.name}\'s ante?\s"
-    ante = gets.strip.to_f
+    # show_wallets
+    ante = @prompt.ask("What is #{player.name}\'s ante?\s", convert: :float)
     max_ante = player.wallet.amount
     out_of_funds(player) if ante > max_ante || max_ante.zero?
     ante
@@ -89,12 +103,12 @@ class Dice
   # @param player [Player] a single player object of the game
   # @return nil
   def out_of_funds(player)
-    puts "\e[2J"
+    clear_screen
     puts "#{player.name.capitalize}!!!\s Your out of Money.".red
     puts 'This doesn\'t matter, we\'ll let you go in debt any!'.green
-    puts "\n\n"
-    puts 'Hit the [Enter] key..'.yellow
-    gets
+    puts "\n"
+    @prompt.ask('Hit the [Enter] key..'.yellow)
+    # gets
   end
 
   # Rolls the dice and gives back some random numbers for each die
@@ -107,13 +121,13 @@ class Dice
   # Displays the values of each die when they were rolled
   # @return nil
   def show_dice
-    puts "Die 1 = #{@die1}\nDie 2 = #{@die2}"
+    @prompt.say("Die 1 = #{@die1}\nDie 2 = #{@die2}".yellow)
   end
 
   # Displays the sum of the two die
   # @return
   def show_sum
-    puts "Dice Sum: #{@die1 + @die2}."
+    @prompt.say("Dice Sum: #{@die1 + @die2}")
   end
 
   # Determines who and when players can join a round of the game
@@ -215,10 +229,13 @@ class Dice
   # round of craps. Also, assigns winnings back into a players wallet
   # @return nil
   def see_bookie
-    winner, earnings = high_points_winner
-    @players[winner][:earnings] = earnings
-    show_winner_info(winner)
-    winner.wallet.amount += earnings
+    winners, earnings = high_points_winner
+    share = earnings/winners.count
+    winners.each do |winner|
+      @players[winner][:earnings] = share
+      winner.wallet.amount += share
+    end
+    show_winner_info(winners)
   end
 
   # Determines who the high points winner is. Removes losers ante from
@@ -227,62 +244,66 @@ class Dice
   # @return winner [Player] a single players object reference
   # @return earnings [Float] dollars earned by winning
   def high_points_winner
-    winner = find_highest_points
+    high_pointers = find_highest_points
     earnings = 0.0
+    winners = []
     @players.each do |player, data|
-      if player == winner
-        if @players[winner][:win]
-          earnings += data[:ante] # gift from the house
+      if high_pointers.include?(player)
+        if @players[player][:win] # eliminate players with zero points
+          winners << player
         else
-          earnings -= data[:ante]
+          player.wallet.amount - data[:ante] # high points but still lost
         end
-      elsif player != winner
+        earnings += data[:ante]
+      else # non-high_point players
         earnings += data[:ante]
         player.wallet.amount -= data[:ante]
       end
     end
-    [winner, earnings]
+    [winners, earnings]
   end
 
   # Determines who the winner or highest points holder is
   # @return winner [Player] single player object of winner
   def find_highest_points
     highest = -1
-    winner = nil
     @players.each do |player, data|
       if data[:points] > highest
         highest = data[:points]
-        winner = player
       end
     end
-    winner
+    @players.select { |player, data| data[:points] == highest }
   end
 
   # Displays the winners informations
   # @param winner [Player] single player object of the winner
   # @return nil
-  def show_winner_info(winner)
-    points = @players[winner][:points]
-    earnings = @players[winner][:earnings]
-    puts "Points Leader:\s#{winner.name}\swith #{points} points and $#{earnings} in earnings"
+  def show_winner_info(winners)
+    winners.each do |winner|
+      points = @players[winner][:points]
+      earnings = @players[winner][:earnings]
+      output = "Points Leader:\s#{winner.name}\swith #{points}\s" +
+        "points and $#{earnings} in earnings"
+      @prompt.say(output)
+    end
   end
 
 end
-#
-# class Player
-#   attr_accessor :name, :age, :gender, :wallet
-#   def initialize(name, age, gender)
-#     @name = name
-#     @age = age
-#     @gender = gender
-#     @wallet = Wallet.new
-#   end
-# end
-#
-# players = [
-#   Player.new('Jennifer', 34, 'female'),
-#   Player.new('Brennick', 21, 'male'),
-#   Player.new('Francis', 34, 'female')
-# ]
-#
-# Dice.new(players)
+
+class Player
+  attr_accessor :name, :age, :gender, :wallet
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+    @wallet = Wallet.new
+  end
+end
+
+players = [
+  Player.new('Jennifer', 34, 'female'),
+  Player.new('Brennick', 21, 'male'),
+  Player.new('Francis', 34, 'female')
+]
+
+Dice.new(players)
