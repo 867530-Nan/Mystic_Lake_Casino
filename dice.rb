b30ab98f59aa39fc8e7476760b7434dbc359bdc6
IMS-Global <brennick.sci@gmail.com>
2017-05-22 19:19:38 -0600

    Dice Class Debugging

--- dice.rb
+++ dice.rb
@@ -14,18 +14,67 @@ class Dice
     @ante = ante
     @players = {}
     players.is_a?(Array) ? join_game(players) : join_game([players])
+    show_menu
+  end
+
+  def show_menu
+    puts "\e[2J"
+    puts '-' * 50
+    puts "\n"
+    puts format '%3s%s', ' ', 'Welcome to Street Craps!'.green
+    puts format '%3s%s', ' ', "\n"
+    puts format '%3s%s', ' ', 'Here are the rules:'
+    puts format '%3s%s', ' ', '1) Set your ante'
+    puts format '%3s%s', ' ', '2) Make a bet (select a combination)'
+    puts format '%3s%s', ' ', '3) Roll the dice'
+    puts format '%3s%s', ' ', '4) Reap your earning for beating the odds'
+    puts format '%3s%s', ' ', '6) Repeat!'
+    puts format '%3s%s', ' ', '5) Call Social Services for financial assistance'
+    puts format '%3s%s', ' ', "\n"
+    puts '   How to Place a Bet'.green
+    puts '   > Pass - Dice sum to 7 or 11'.yellow
+    puts '   > Don\'t Pass - Dice sum to 2, 3, or 12'.yellow
+    puts '   > Points - Dice sum to 4, 5, 6, 8, 9, or 10'.yellow
+    puts '   * Player loses with all other sums'.yellow
+    puts "\n"
+    puts format '%3s%s', ' ', 'Hit the [Enter Key] to try your luck!'
+    gets
     play_round
   end
 
-  def place_bet
-    print "What is your bet? [1 for pass, 2 for don\'t pass, 3 for points]\s"
+  def show_wallets
+    puts "\e[2J"
+    puts "++++++++++++++++++ Players Wallets ++++++++++++++++++"
+    @players.each do |player, data|
+      points = @players[player][:points]
+      wallet = player.wallet.amount
+      puts "#{player.name}\shas\s$#{wallet} and #{points} points"
+    end
+    puts "\n"
+  end
+
+  def place_bet(player)
+    print "What is #{player.name}\'s bet? " +
+      "[ Enter 1 for pass, 2 for don\'t pass, 3 for points]\s"
     ans = gets.strip.to_i
     ans
   end
 
-  def place_ante
+  def place_ante(player)
+    show_wallets
     print "What is your ante?\s"
-    gets.strip.to_f
+    ante = gets.strip.to_f
+    max_ante = player.wallet.amount
+    out_of_funds(player) if ante > max_ante && !max_ante.zero?
+    ante
+  end
+
+  def out_of_funds(player)
+    puts "\e[2J"
+    puts "#{player.name.capitalize}!!!\s Your out of Money.".red
+    puts 'Please Visit the closest ATM and Load Your Wallet to continue.'.green
+    puts "\n\n"
+    puts "Hit the [Enter] key..".yellow
   end
 
   def roll
@@ -34,11 +83,11 @@ class Dice
   end
 
   def show_dice
-    print "Die 1: ", @die1, " Die 2: ", @die2
+    puts "Die 1 = #{@die1}\nDie 2 = #{@die2}"
   end
 
   def show_sum
-    print "Sum of dice is: ", @die1 + @die2, ".\n"
+    puts "Dice Sum: #{@die1 + @die2}."
   end
 
   def join_game(players)
@@ -62,36 +111,42 @@ class Dice
   end
 
   def win_lose?(player, bet)
-    case @die1 + @die2
-    when 7 || 11 # pass
+    show_sum
+    show_dice
+    sum = @die1 + @die2
+    case sum.to_s
+    when /[7|11]/ # pass
       bet == 1 ? winner(player, 3) : loser(player, 3)
-    when 2 || 3 || 12 # don't pass
+    when /[2|3|12]/ # don't pass
       bet == 2 ? winner(player, 2) : loser(player, 2)
-    when 4 || 5 || 6 || 8 || 9 || 10 # carry over
+    when /[4|5|6|8|9|10]/ # carry over
       bet == 3 ? winner(player) : loser(player)
     else
       loser(player)
     end
   end
 
-  def winner(player, point = 1)
-    @players[player][:points] += point
+  def winner(player, points = 1)
+    @players[player][:points] += points
   end
 
   def loser(player, points = 1)
-    if @players[player][:points] - points >= 0
+    if @players[player][:points] - points > 0
       @players[player][:points] -= points
+    elsif @players[player][:points] > 0
+      @players[player][:points] = 0
     end
   end
 
   def play_round(rounds = 1)
+    show_wallets
     @players.keys.each do |player|
-      @players[player][:ante] = place_ante
+      @players[player][:ante] = place_ante(player)
     end
     (1..rounds).each {
       @players.keys.each do |player|
         roll
-        win_lose?(player, place_bet)
+        win_lose?(player, place_bet(player))
       end
     }
     see_bookie
@@ -101,12 +156,12 @@ class Dice
   def quit_game?
     print "Quit?\s(y/N)\s"
     yN = gets
-    yN =~ /[y|Y|yes|Yes]/ ? exit : play_round
+    yN =~ /[y|Y|yes|Yes]/ ? return : play_round
   end
 
   def see_bookie
     winner, earnings = high_points_winner
-    @players[winner][:earnings] = earnings
+    @players[winner][:earnings] += earnings
     show_winner_info(winner)
   end
 
@@ -117,7 +172,9 @@ class Dice
     @players.each do |player, data|
       if data[:points] == highest_points
         winner = player
-        earnings += @players[winner][:ante] if @players.keys.count == 1
+        if @players.keys.count == 1 && highest_points > 0
+          earnings -= @players[winner][:ante]
+        end
       else
         earnings += data[:ante]
         player.wallet.amount -= data[:ante]
@@ -127,7 +184,7 @@ class Dice
   end
 
   def find_highest_points
-    highest = 0
+    highest = -1
     @players.values.each do |data|
       highest = data[:points] if data[:points] > highest
     end
@@ -137,7 +194,7 @@ class Dice
   def show_winner_info(winner)
     points = @players[winner][:points]
     earnings = @players[winner][:earnings]
-    puts "Points Leader:\s#{winner.name}\swith #{points} points and $#{earnings}"
+    puts "Points Leader:\s#{winner.name}\swith #{points} points and $#{earnings} earnings"
   end
 
 end
